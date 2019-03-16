6de86d1a20963843c6e796e67f172bf6c609010c
kakakos <kakaarithmatic@gmail.com>
2019-03-17 00:12:39 +0700

    added Ex5_1 , Ex5_2

---
+++ Exercise5_1_Yotin_S.py
@@ -0,0 +1,11 @@
+a = 75
+b = 15
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+f = int(f)
+print(a,'+',b,'=',c)
+print(a,'-',b,'=',d)
+print(a,'*',b,'=',e)
+print(a,'/',b,'=',f)
---
+++ Exercise_5_2_Yotin_S.py
@@ -0,0 +1,5 @@
+v = int(input('v = ความเร็ว (m/s หรือ km/hr) (v >= 1) : '))
+t = int(input('t = เวลา (s หรือ hr) (t >= 1) : '))
+print('v =',v ,',','t =',t)
+c = v / t
+print(int(c) , str('km/hr'))
